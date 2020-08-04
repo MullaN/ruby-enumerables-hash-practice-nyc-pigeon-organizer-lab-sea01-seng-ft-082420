494e7d9d9b7a5ea6f3d8b48836678542ac080e85
Andrew Mullan <admullan@gmail.com>
2020-08-04 06:51:30 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,6 +2,8 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   # write your code here!
-  pigeon_hash = data[:gender][:male].merge(data[:gender][:female])
-  binding.pry
+  pigeon_hash = {}
+  data[:gender].each do |key, value|
+
+  end
 end
