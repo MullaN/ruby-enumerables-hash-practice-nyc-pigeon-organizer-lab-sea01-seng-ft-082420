cd99c80d0fc3ec895706b58bb790739a6fb81d9f
Andrew Mullan <admullan@gmail.com>
2020-08-04 07:20:35 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_hash = {}
+  data.each do |desc_key, desc_hash|
+    data[desc_key].each do |key, value|
+      value.each do |name|
+        if !pigeon_hash[name]
+          pigeon_hash[name] = {
+            color: [],
+            gender: [],
+            lives: []
+          }
+        end
+        pigeon_hash[name][desc_key] << key.to_s
+      end
+    end
+  end
+  return pigeon_hash
 end
