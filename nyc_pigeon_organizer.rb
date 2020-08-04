29a11ebb769b2119531fb9a60b4689e76e2e18a9
Andrew Mullan <admullan@gmail.com>
2020-08-04 07:00:20 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,12 +4,17 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   pigeon_hash = {}
   data[:gender].each do |key, value|
-    value.each do |element|
+    value.each do |name|
       pigeon_hash[name] = {
         color: [],
         gender: [key.to_s],
         lives: []
       }
+    end
+  end
+  data[:color].each do |key, value|
+    value.each do |name|
+      pigeon_hash[name][:color] << key
     end
   end
   return pigeon_hash
