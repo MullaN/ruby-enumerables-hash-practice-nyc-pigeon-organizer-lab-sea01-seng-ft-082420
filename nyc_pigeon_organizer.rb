41e1591131bbc10728185faabadd14d00d5970f6
Andrew Mullan <admullan@gmail.com>
2020-08-04 07:08:45 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,23 @@
 require 'pry'
 
+def nyc_pigeon_organizer(data)
+  pigeon_hash = {}
+  data.each do |desc_key, desc_hash|
+    data[desc_key].each do |key, value|
+      value.each do |name|
+        if pigeon_hash[name]
+          pigeon_hash[name] = {
+            color: [],
+            gender: [],
+            lives: []
+          }
+        pigeon_hash[name][desc] << key.to_s
+      end
+    end
+  end
+  return pigeon_hash
+end
+
 def nyc_pigeon_organizer_bad(data)
   # write your code here!
   pigeon_hash = {}
@@ -20,24 +38,6 @@ def nyc_pigeon_organizer_bad(data)
   data[:lives].each do |key, value|
     value.each do |name|
       pigeon_hash[name][:lives] << key
-    end
-  end
-  return pigeon_hash
-end
-
-def nyc_pigeon_organizer(data)
-  pigeon_hash = {}
-  data.each do |desc_key, desc_hash|
-    data[desc_key].each do |key, value|
-      value.each do |name|
-        if pigeon_hash[name]
-          pigeon_hash[name] = {
-            color: [],
-            gender: [],
-            lives: []
-          }
-        pigeon_hash[name][desc] << key.to_s
-      end
     end
   end
   return pigeon_hash
