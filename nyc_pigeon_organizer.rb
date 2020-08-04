b0653efb3b6d1eb7c16cda51f3b0b78a4bfdd2f2
Andrew Mullan <admullan@gmail.com>
2020-08-04 07:10:16 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -18,29 +18,29 @@ def nyc_pigeon_organizer(data)
   return pigeon_hash
 end
 
-def nyc_pigeon_organizer_bad(data)
-  # write your code here!
-  pigeon_hash = {}
-  data[:gender].each do |key, value|
-    value.each do |name|
-      pigeon_hash[name] = {
-        color: [],
-        gender: [key.to_s],
-        lives: []
-      }
-    end
-  end
-  data[:color].each do |key, value|
-    value.each do |name|
-      pigeon_hash[name][:color] << key.to_s
-    end
-  end
-  data[:lives].each do |key, value|
-    value.each do |name|
-      pigeon_hash[name][:lives] << key
-    end
-  end
-  return pigeon_hash
-end
+#def nyc_pigeon_organizer_bad(data)
+#  # write your code here!
+#  pigeon_hash = {}
+#  data[:gender].each do |key, value|
+#    value.each do |name|
+#      pigeon_hash[name] = {
+#        color: [],
+#        gender: [key.to_s],
+#        lives: []
+#      }
+#    end
+#  end
+#  data[:color].each do |key, value|
+#    value.each do |name|
+#      pigeon_hash[name][:color] << key.to_s
+#    end
+#  end
+#  data[:lives].each do |key, value|
+#    value.each do |name|
+#      pigeon_hash[name][:lives] << key
+#    end
+#  end
+#  return pigeon_hash
+#end
 
 binding.pry
